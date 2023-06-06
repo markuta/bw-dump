bb9348189977660d1beb8d159f814f9c6ead2cc2
Naz Markuta <markuta@users.noreply.github.com>
2023-06-06 20:49:29 +0100

    Added more strings to exclude list

--- constants.go
+++ constants.go
@@ -2,6 +2,26 @@ package main
 
 import "syscall"
 
+// Known Bitwarden strings that may match
+// the Regex pattern. I need to add more!
+var StaticBWStrings = []string{
+	"Refused to evaluate",
+	"Refused to compile",
+	"/index.html",
+	"Open_Sans-normal-",
+	"ch-ua-full-version",
+	"refresh_token",
+	"ResourcePoolMemoryUsage.Renderer",
+	"codeUtf8",
+	"data:image/svg+xml",
+	"command\"",
+	"aultTimeoutAction",
+	"[Email MasterPassword ] x64",
+	"app/main.js",
+	"no-items-image",
+	"HighResolutionTimerUsage",
+}
+
 const (
 	ChromeEXEName             = "chrome.exe"
 	MSEdgeEXEName             = "msedge.exe"
